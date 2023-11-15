712e7a5dfefeade33e4df823c67c46bbeb6eebeb
tamara <tamaraksaid@gmail.com>
2023-11-15 20:07:50 +0100

    Download Genome and Store in proper place

--- scripts/pipline.sh
+++ scripts/pipline.sh
@@ -1,2 +1,3 @@
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+mkdir res/genome
+wget -O ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 
