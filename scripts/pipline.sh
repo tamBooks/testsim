948d961c54733e8430ed0cad310543819323c4c9
tamara <tamaraksaid@gmail.com>
2023-11-15 20:27:40 +0100

    Renaming pipline

--- scripts/pipline.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p res/genome
-wget -O ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
-
-# indexing code goes here
-
-for  $sampleid in $(get sample ids)
-do
-	#call analyze_sample for each sampleid
-done
