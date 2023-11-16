245ba6852a30ba331b16568fc6c437da82455cc6
tamara <tamaraksaid@gmail.com>
2023-11-16 18:50:59 +0100

    Cutting adapter

--- scripts/run_pipline.sh
+++ scripts/run_pipline.sh
@@ -9,6 +9,11 @@ mv GCF_000005845.2_ASM584v2_genomic.fna.gz res/genome/
 mv res/genome/GCF_000005845.2_ASM584v2_genomic.fna.gz res/genome/ecoli.fasta.gz  
 gunzip res/genome/ecoli.fasta.gz
 
+# Cut the adapters of the sequences
+
+sampleid=$1
+echo cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -o out/cutadapt/${sampleid}_1.trimmed.fastq.gz -p out/cutadapt/${sampleid}_2.trimmed.fastq.gz data/${sampleid}_1.fastq.gz data/${sampleid}_2.fastq.gz 
+
 # indexing code goes here
 echo "Running STAR indexing"
 mkdir -p res/genome/star_index
